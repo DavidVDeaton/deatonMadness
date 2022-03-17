56f2ed2e4ba79790595883d74383d448f655e9a7
David Deaton <dideater@gmail.com>
2022-03-17 10:49:16 -0700

    initial commit

---
+++ players.js
@@ -0,0 +1,268 @@
+var david = $("<img>", {
+    class: "small-circle",
+    alt: "David",
+    src: "assets/profile/david.png"
+});
+
+var dave = $("<img>", {
+    class: "small-circle",
+    alt: "Dave",
+    src: "assets/profile/dave.png"
+});
+
+var april = $("<img>", {
+    class: "small-circle",
+    alt: "April",
+    src: "assets/profile/april.png"
+});
+
+var caleb = $("<img>", {
+    class: "small-circle",
+    alt: "Caleb",
+    src: "assets/profile/caleb.png"
+});
+
+var ethan = $("<img>", {
+    class: "small-circle",
+    alt: "Ethan",
+    src: "assets/profile/ethan.png"
+});
+
+var katie = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/katie.jpg"
+});
+
+var matt = $("<img>", {
+    class: "small-circle",
+    alt: "Katie",
+    src: "assets/profile/matt.png"
+});
+
+var paige = $("<img>", {
+    class: "small-circle",
+    alt: "Paige",
+    src: "assets/profile/paige.png"
+});
+
+var noah = $("<img>", {
+    class: "small-circle",
+    alt: "Noah",
+    src: "assets/profile/noah.png"
+});
+
+var reed = $("<img>", {
+    class: "small-circle",
+    alt: "Reed",
+    src: "assets/profile/reed.png"
+});
+
+var dennis = $("<img>", {
+    class: "small-circle",
+    alt: "Dennis",
+    src: "assets/profile/dennis.png"
+});
+
+var susan = $("<img>", {
+    class: "small-circle",
+    alt: "Susan",
+    src: "assets/profile/susan.png"
+});
+
+var hyrum = $("<img>", {
+    class: "small-circle",
+    alt: "Hyrum",
+    src: "assets/profile/hyrum.png"
+});
+
+var levi = $("<img>", {
+    class: "small-circle",
+    alt: "Levi",
+    src: "assets/profile/levi2.jpg"
+});
+
+var rachel = $("<img>", {
+    class: "small-circle",
+    alt: "Rachel",
+    src: "assets/profile/rachel.png"
+});
+
+var shea = $("<img>", {
+    class: "small-circle",
+    alt: "Shea",
+    src: "assets/profile/shea.png"
+});
+
+var christine = $("<img>", {
+    class: "small-circle",
+    alt: "Christine",
+    src: "assets/profile/christine.png"
+});
+
+var tim = $("<img>", {
+    class: "small-circle",
+    alt: "Tim",
+    src: "assets/profile/tim.png"
+});
+
+var nick = $("<img>", {
+    class: "small-circle",
+    alt: "Nick",
+    src: "assets/profile/nick.png"
+});
+
+var luke = $("<img>", {
+    class: "small-circle",
+    alt: "Luke",
+    src: "assets/profile/luke.png"
+});
+
+var sabrina = $("<img>", {
+    class: "small-circle",
+    alt: "Sabrina",
+    src: "assets/profile/sabrina.jpg"
+});
+
+var miriam = $("<img>", {
+    class: "small-circle",
+    alt: "Miriam",
+    src: "assets/profile/miriam.png"
+});
+
+$(".david").append(david);
+$(".dave").append(dave);
+$(".april").append(april);
+$(".caleb").append(caleb);
+$(".ethan").append(ethan);
+$(".katie").append(katie);
+$(".matt").append(matt);
+$(".paige").append(paige);
+$(".noah").append(noah);
+$(".reed").append(reed);
+$(".dennis").append(dennis);
+$(".susan").append(susan);
+$(".hyrum").append(hyrum);
+$(".rachel").append(rachel);
+$(".levi").append(levi);
+$(".shea").append(shea);
+$(".nick").append(nick);
+$(".luke").append(luke);
+$(".tim").append(tim);
+$(".christine").append(christine);
+$(".miriam").append(miriam);
+
+
+
+// var davidMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "David",
+//     src: "assets/profile/david.jpg"
+// });
+
+// var daveMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Dave",
+//     src: "assets/profile/dave.jpg"
+// });
+
+// var aprilMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "April",
+//     src: "assets/profile/april.jpg"
+// });
+
+// var calebMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Caleb",
+//     src: "assets/profile/caleb.jpg"
+// });
+
+// var ethanMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Ethan",
+//     src: "assets/profile/ethan.png"
+// });
+
+// var katieMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Katie",
+//     src: "assets/profile/katie.jpg"
+// });
+
+// var mattMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Katie",
+//     src: "assets/profile/matt.jpg"
+// });
+
+// var paigeMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Paige",
+//     src: "assets/profile/paige.jpg"
+// });
+
+// var noahMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Noah",
+//     src: "assets/profile/noah.jpg"
+// });
+
+// var reedMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Reed",
+//     src: "assets/profile/reed.jpg"
+// });
+
+// var dennisMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Dennis",
+//     src: "assets/profile/dennis.jpg"
+// });
+
+// var susanMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Susan",
+//     src: "assets/profile/susan.png"
+// });
+
+// var hyrumMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Hyrum",
+//     src: "assets/profile/hyrum2.jpg"
+// });
+
+// var leviMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Levi",
+//     src: "assets/profile/levi2.jpg"
+// });
+
+// var rachelMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Rachel",
+//     src: "assets/profile/rachel.jpg"
+// });
+
+// var sheaMed = $("<img>", {
+//     class: "med-circle",
+//     alt: "Shea",
+//     src: "assets/profile/shea.png"
+// });
+
+// $(".david").append(davidMed);
+// $(".dave").append(daveMed);
+// $(".april").append(aprilMed);
+// $(".caleb").append(calebMed);
+// $(".ethan").append(ethanMed);
+// $(".katie").append(katieMed);
+// $(".matt").append(mattMed);
+// $(".paige").append(paigeMed);
+// $(".noah").append(noahMed);
+// $(".reed").append(reedMed);
+// $(".dennis").append(dennisMed);
+// $(".susan").append(susanMed);
+// $(".hyrum").append(hyrumMed);
+// $(".rachel").append(rachelMed);
+// $(".levi").append(leviMed);
+// $(".shea").append(sheaMed);
---
+++ scores.js
@@ -0,0 +1,487 @@
+var participants = 20;
+
+
+//     // Game 1
+//     var gm1 = "";
+//     var gm1Picks = 19;
+//     var colost1 = 0;
+//     var mich1 = 0;
+//     if (gm1="colost") {
+//         colost1 = participants/gm1Picks;
+//     }
+//     else if (gm1="mich"){
+//         mich1 = participants/(participants-gm1Picks);
+//     }
+//     else {
+//         colost1 = 0;
+//         mich1 = 0;
+//     }
+
+// var gm2 = "";
+// var gm2Picks = 19;
+
+// var gm3 = "";
+// var gm3Picks = 19;
+// var gm4 = "";
+// var gm4Picks = 19;
+// var gm5 = "";
+// var gm5Picks = 19;
+// var gm6 = "";
+// var gm6Picks = 19;
+// var gm7 = "";
+// var gm7Picks = 19;
+// var gm8 = "";
+// var gm8Picks = 19;
+// var gm9 = "";
+// var gm9Picks = 19;
+// var gm10 = "";
+// var gm10icks = 19;
+// var gm11 = "";
+// var gm11Picks = 19;
+// var gm12 = "";
+// var gm12Picks = 19;
+// var gm1 = "";
+// var gm1Picks = 19;
+// var gm2 = "";
+// var gm2Picks = 19;
+// var gm3 = "";
+// var gm3Picks = 19;
+// var gm4 = "";
+// var gm4Picks = 19;
+// var gm5 = "";
+// var gm5Picks = 19;
+// var gm6 = "";
+// var gm6Picks = 19;
+// var gm7 = "";
+// var gm7Picks = 19;
+// var gm8 = "";
+// var gm8Picks = 19;
+// var gm9 = "";
+// var gm9Picks = 19;
+// var gm10 = "";
+// var gm10icks = 19;
+// var gm11 = "";
+// var gm11Picks = 19;
+// var gm12 = "";
+// var gm12Picks = 19;
+
+//     // West Round 1
+
+// var zaga1Picks = 19;
+// var zaga1 = participants/zaga1Picks;
+// var geost1 = participants/(participants-zaga1Picks);
+
+// var boise1Picks = 19;
+// var boise1 = participants/boise1Picks;
+// var mem1 = participants/(participants-boise1Picks);
+
+// var conn1Picks = 19;
+// var conn1 = participants/conn1Picks;
+// var nmst1 = participants/(participants-conn1Picks);
+
+// var ark1Picks = 19;
+// var ark1 = participants/ark1Picks;
+// var verm1 = participants/(participants-ark1Picks);
+
+// var bama1Picks = 19;
+// var bama1 = participants/bama1Picks;
+// var nd1 = participants/(participants-bama1Picks);
+
+// var textech1Picks = 19;
+// var textech1 = participants/textech1Picks;
+// var montst1 = participants/(participants-textech1Picks);
+
+// var michst1Picks = 19;
+// var michst1 = participants/michst1Picks;
+// var david1 = participants/(participants-michst1Picks);
+
+// var duke1Picks = 19;
+// var duke1 = participants/duke1Picks;
+// var csuf1 = participants/(participants-duke1Picks);
+
+//     // South Round 1
+
+// var az1Picks = 19;
+// var az1 = participants/az1Picks;
+// var wrst1 = participants/(participants-az1Picks);
+
+// var seton1Picks = 19;
+// var seton1 = participants/seton1Picks;
+// var tcu1 = participants/(participants-seton1Picks);
+
+// var hou1Picks = 19;
+// var hou1 = participants/hou1Picks;
+// var uab1 = participants/(participants-hou1Picks);
+
+// var ill1Picks = 19;
+// var ill1 = participants/ill1Picks;
+// var chat1 = participants/(participants-ill1Picks);
+
+// var tenn1Picks = 19;
+// var tenn1 = participants/tenn1Picks;
+// var longw1 = participants/(participants-tenn1Picks);
+
+// var ohiost1Picks = 19;
+// var ohiost1 = participants/ohiost1Picks;
+// var loy1 = participants/(participants-ohiost1Picks);
+
+// var nova1Picks = 19;
+// var nova1 = participants/nova1Picks;
+// var del1 = participants/(participants-nova1Picks);
+
+
+//     // East Round 1
+
+// var bay1Picks = 19;
+// var bay1 = participants/bay1Picks;
+// var norst1 = participants/(participants-bay1Picks);
+
+// var unc1Picks = 19;
+// var unc1 = participants/unc1Picks;
+// var marq1 = participants/(participants-unc1Picks);
+
+// var stmarys1Picks = 19;
+// var stmarys1 = participants/stmarys1Picks;
+// var ind1 = participants/(participants-stmarys1Picks);
+
+// var ucla1Picks = 19;
+// var ucla1 = participants/ucla1Picks;
+// var akron1 = participants/(participants-ucla1Picks);
+
+// var tex1Picks = 19;
+// var tex1 = participants/tex1Picks;
+// var vt1 = participants/(participants-tex1Picks);
+
+// var pur1Picks = 19;
+// var pur1 = participants/pur1Picks;
+// var yale1 = participants/(participants-pur1Picks);
+
+// var murr1Picks = 19;
+// var murr1 = participants/murr1Picks;
+// var sf1 = participants/(participants-murr1Picks);
+
+// var uk1Picks = 19;
+// var uk1 = participants/uk1Picks;
+// var stpete1 = participants/(participants-uk1Picks);
+
+//    // Midwest Round 1
+
+// var kan1Picks = 19;
+// var kan1 = participants/kan1Picks;
+// var txso1 = participants/(participants-kan1Picks);
+
+// var sdsu1Picks = 19;
+// var sdsu1 = participants/sdsu1Picks;
+// var creight1 = participants/(participants-sdsu1Picks);
+
+// var iowa1Picks = 19;
+// var iowa1 = participants/iowa1Picks;
+// var rich1 = participants/(participants-iowa1Picks);
+
+// var prov1Picks = 19;
+// var prov1 = participants/prov1Picks;
+// var sdakst1 = participants/(participants-prov1Picks);
+
+// var lsu1Picks = 19;
+// var lsu1 = participants/lsu1Picks;
+// var iowast1 = participants/(participants-lsu1Picks);
+
+// var wisc1Picks = 19;
+// var wisc1 = participants/wisc1Picks;
+// var col1 = participants/(participants-wisc1Picks);
+
+// var usc1Picks = 19;
+// var usc1 = participants/usc1Picks;
+// var miami1 = participants/(participants-usc1Picks);
+
+// var aub1Picks = 19;
+// var aub1 = participants/aub1Picks;
+// var jackst1 = participants/(participants-aub1Picks);
+
+var davidScore = 
+                        // West Round 1
+    zaga1 + boise1 + nmst1 + ark1 + bama1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + del1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + prov1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+var daveScore =  
+                        // West Round 1
+    zaga1 + boise1 + conn1 + ark1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + colost1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + sdakst1 + lsu1 + wisc1 + miami1 + aub1
+;
+
+var aprilScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + uab1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + sdakst1 + iowast1 + wisc1 + miami1 + aub1
+;
+
+var hyrumScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + bama1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + prov1 + lsu1 + wisc1 + usc1 + aub1
+;
+
+var mattScore =  
+                        // West Round 1
+    zaga1 + boise1 + conn1 + verm1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + uab1 + ill1 + colost1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + prov1 + iowast1 + wisc1 + miami1 + aub1
+;
+
+var timScore =  
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + nd1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + sdakst1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+// var katieScore = 
+//                         // West Round 1
+//     zaga1 + boise1 + nmst1 + ark1 + bama1 + textech1 + michst1 + duke1 +
+//                         // South Round 1
+//     az1 + seton1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + del1 + 
+//                         // East Round 1
+//     bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+//                         // Midwest Round 1
+//     kan1 + creight1 + iowa1 + prov1 + iowast1 + wisc1 + usc1 + aub1
+// ;
+
+var noahScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + nd1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + stmarys1 + ucla1 + vt1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + prov1 + iowast1 + wisc1 + miami1 + aub1
+;
+
+var miriamScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + nd1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + prov1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+var susanScore = 
+                        // West Round 1
+    zaga1 + boise1 + conn1 + ark1 + nd1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + chat1 + colost1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + prov1 + lsu1 + wisc1 + usc1 + aub1
+;
+
+var sheaScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + colost1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + prov1 + iowast1 + wisc1 + miami1 + aub1
+;
+
+var leviScore = 
+                        // West Round 1
+    zaga1 + mem1 + nmst1 + ark1 + nd1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + chat1 + colost1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + ind1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + prov1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+var calebScore =  
+                        // West Round 1
+    zaga1 + mem1 + nmst1 + ark1 + nd1 + montst1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + uab1 + chat1 + mich1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + ind1 + ucla1 + vt1 + yale1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + sdakst1 + iowast1 + col1 + miami1 + aub1
+;
+
+var ethanScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + verm1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + hou1 + chat1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + ind1 + ucla1 + vt1 + yale1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + sdakst1 + lsu1 + wisc1 + miami1 + aub1
+;
+
+var dennisScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + nd1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + hou1 + ill1 + colost1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + prov1 + lsu1 + wisc1 + usc1 + aub1
+;
+
+var paigeScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + bama1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + sdakst1 + lsu1 + wisc1 + miami1 + aub1
+;
+
+var reedScore = 
+                        // West Round 1
+    geost1 + mem1 + conn1 + ark1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + colost1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + ind1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    txso1 + creight1 + iowa1 + sdakst1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+var rachelScore = 
+                        // West Round 1
+    zaga1 + mem1 + nmst1 + ark1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + uab1 + chat1 + colost1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + marq1 + ind1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + rich1 + prov1 + iowast1 + wisc1 + miami1 + jackst1
+;
+
+var christineScore = 
+                        // West Round 1
+    zaga1 + boise1 + conn1 + verm1 + nd1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + ind1 + ucla1 + tex1 + yale1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + rich1 + prov1 + lsu1 + wisc1 + usc1 + aub1
+;
+
+var lukeScore = 
+                        // West Round 1
+    zaga1 + boise1 + conn1 + verm1 + bama1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + uab1 + ill1 + mich1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + vt1 + pur1 + murr1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + sdakst1 + iowast1 + wisc1 + miami1 + aub1
+;
+
+var nickScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + ark1 + nd1 + textech1 + michst1 + duke1 +
+                        // South Round 1
+    az1 + seton1 + hou1 + ill1 + mich1 + tenn1 + ohiost1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + stmarys1 + ucla1 + tex1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + sdsu1 + iowa1 + sdakst1 + lsu1 + wisc1 + miami1 + aub1
+;
+
+var sabrinaScore = 
+                        // West Round 1
+    zaga1 + mem1 + conn1 + verm1 + bama1 + textech1 + david1 + duke1 +
+                        // South Round 1
+    az1 + tcu1 + hou1 + ill1 + mich1 + tenn1 + loy1 + nova1 + 
+                        // East Round 1
+    bay1 + unc1 + ind1 + ucla1 + vt1 + pur1 + sf1 + uk1 + 
+                        // Midwest Round 1
+    kan1 + creight1 + iowa1 + sdakst1 + iowast1 + wisc1 + usc1 + aub1
+;
+
+var nsabrina = sabrinaScore.toFixed(2);
+var nnick = nickScore.toFixed(2);
+var nluke = lukeScore.toFixed(2);
+var ndavid = davidScore.toFixed(2);
+var ndennis = dennisScore.toFixed(2);
+var nsusan = susanScore.toFixed(2);
+var ndave = daveScore.toFixed(2);
+var napril  = aprilScore.toFixed(2);
+var ncaleb = calebScore.toFixed(2);
+var nethan = ethanScore.toFixed(2);
+var nlevi = leviScore.toFixed(2);
+var nhyrum = hyrumScore.toFixed(2);
+var nrachel = rachelScore.toFixed(2);
+var npaige = paigeScore.toFixed(2);
+var nmatt = mattScore.toFixed(2);
+var nkatie = katieScore.toFixed(2);
+var nreed = reedScore.toFixed(2);
+var nnoah = noahScore.toFixed(2);
+var nshea = sheaScore.toFixed(2);
+var nchristine = christineScore.toFixed(2);
+var ntim = timScore.toFixed(2);
+var nmiriam = miriamScore.toFixed(2);
+
+$("#davidScore").text(ndavid);
+$("#dennisScore").text(ndennis);
+$("#susanScore").text(nsusan);
+$("#daveScore").text(ndave);
+$("#aprilScore").text(napril);
+$("#calebScore").text(ncaleb);
+$("#ethanScore").text(nethan);
+$("#leviScore").text(nlevi);
+$("#hyrumScore").text(nhyrum);
+$("#rachelScore").text(nrachel);
+$("#paigeScore").text(npaige);
+$("#mattScore").text(nmatt);
+$("#katieScore").text(nkatie);
+$("#reedScore").text(nreed);
+$("#noahScore").text(nnoah);
+$("#sheaScore").text(nshea);
+$("#lukeScore").text(nluke);
+$("#nickScore").text(nnick);
+$("#sabrinaScore").text(nsabrina);
+$("#christineScore").text(nchristine);
+$("#timScore").text(ntim);
+$("#miriamScore").text(nmiriam);
