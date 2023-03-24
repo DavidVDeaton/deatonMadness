6b9be006568ab5c86570cd9a04625b1121b64ba6
David Deaton <dideater@gmail.com>
2023-03-24 00:08:54 -0400

    gonzaga win

--- scores.js
+++ scores.js
@@ -22,7 +22,7 @@ let calebPicks = {
 
 let suzyPicks = {
     name: "suzy",
-    medal: "",
+    medal: "bronze",
     score: 0,
     picks: [
         "01s","08s","05s","04s","06s","03s","10s","02s",
@@ -132,7 +132,7 @@ let noahPicks = {
 
 let davidPicks = {
     name: "david",
-    medal: "bronze",
+    medal: "",
     score: 0,
     picks: [
         "01s","08s","05s","13s","11s","03s","10s","02s",
@@ -896,7 +896,7 @@ let gm55 = {
 let gm56 = {
     team1:"02w",
     team2:"03w",
-    winner: "",
+    winner: "03w",
     fav:[],
     und:[],
     score:0
